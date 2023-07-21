2d81a0ef6ecd34af326f9d935a7684a5233209fe
Sadiq <sadiq.said@student.moringaschool.com>
2023-07-21 17:07:46 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+// Function expression called 'divide'
+const divide = function () {
+  return 2000 / 100;
+};
+
+// Arrow function called 'square'
+const square = (num) => {
+  return num * num;
+};
+
+// Arrow function called 'add'
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+// Test the functions
+console.log(divide()); 
+console.log(square(5)); 
+console.log(add(3, 7)); 
